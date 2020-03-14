7aa72318aa9a6cb767b256fa7482d8e14cc6e5a8
wenhai <wenhai@ky-tech.com.cn>
2020-03-14 18:23:58 +0800

    feat: 新增 mock 服务

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,29 +2,37 @@ const mockjs = require('mockjs')
 const delay = require('mocker-api/utils/delay')
 const DELAY_TIMER = 1000
 
-const data = {
-  'GET /api/user': {
-    code: 0,
-    msg: '成功',
-    data: {
-      rows: [
-        {
-          id: 1,
-          name: '刘德华2'
-        },
-        {
-          'id': 2,
-          'name': '张学友'
-        }
-      ]
-    }
-  },
-  'POST /api/list': (req, res) => {
-    let mock = mockjs.mock({
-      'list|10-100': 1
-    })
+const json = (mock) => {
+  return (req, res) => {
+    req.body && console.log(req.body)
     res.send(mock)
   }
+}
+
+const data = {
+  'GET /user': json(
+    {
+      code: 0,
+      msg: '成功',
+      data: {
+        rows: [
+          {
+            id: 1,
+            name: '刘德华2'
+          },
+          {
+            'id': 2,
+            'name': '张学友'
+          }
+        ]
+      }
+    }
+  ),
+  'POST /list': json(
+    mockjs.mock({
+      'list|10-100': 1
+    })
+  )
 }
 
 module.exports = delay(data, DELAY_TIMER)
